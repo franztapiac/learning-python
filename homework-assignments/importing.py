8705237481051f33888e7989829c902764192b85
Franz Tapia Chaca <franz.tapia-chaca@outlook.com>
2021-05-30 00:30:34 +0200

    Update importing.py

--- homework-assignments/importing.py
+++ homework-assignments/importing.py
@@ -1,10 +1,10 @@
+# Programming Course: Python is Easy by pirple.com
 # Homework assignment: Importing
-# Class: "Python is Easy" from pirple.com
 # Written by Franz Tapia Chaca
-# Last updated 16 April 2021
+# Last updated 30 May 2021
 
 # Learning how to extensively use matplotlib
-# Executing examples provided at  through example code provided at https://matplotlib.org/stable/tutorials/index.html
+# Executing examples provided at https://matplotlib.org/stable/tutorials/index.html
 
 ##############################
 # Learning to use MATPLOTLIB #
@@ -52,27 +52,61 @@ import numpy as np
 #   2) pyplot-style: pyplot automatically creates and manages figures and axes. You use pyplot functions for plotting
 
 # Object-oriented style
-x = np.linspace(0, 2, 100)  # 100 samples [0, 2], inclusive
-
-fig, ax = plt.subplots()
-ax.plot(x, x, label='linear')
-ax.plot(x, x**2, label='quadratic')
-ax.plot(x, x**3, label='cubic')
-ax.set_xlabel('x label')
-ax.set_ylabel('y label')
-ax.set_title('Simple plot')
-ax.legend()
-plt.show()
-
-# Pyplot-style
-x = np.linspace(0, 2, 100)
-plt.plot(x, x, label='linear')  # data plotted on implicit axes
-plt.plot(x, x**2, label='quadratic')
-plt.plot(x, x**3, label='cubic')
-plt.xlabel('x label')
-plt.ylabel('y label')
-plt.title('Simple plot')
-plt.legend()
-plt.show()
+# x = np.linspace(0, 2, 100)  # 100 samples [0, 2], inclusive
+#
+# fig, ax = plt.subplots()
+# ax.plot(x, x, label='linear')
+# ax.plot(x, x**2, label='quadratic')
+# ax.plot(x, x**3, label='cubic')
+# ax.set_xlabel('x label')
+# ax.set_ylabel('y label')
+# ax.set_title('Simple plot')
+# ax.legend()
+# plt.show()
+#
+# # Pyplot-style
+# x = np.linspace(0, 2, 100)
+# plt.plot(x, x, label='linear')  # data plotted on implicit axes
+# plt.plot(x, x**2, label='quadratic')
+# plt.plot(x, x**3, label='cubic')
+# plt.xlabel('x label')
+# plt.ylabel('y label')
+# plt.title('Simple plot')
+# plt.legend()
+# plt.show()
 
 # 3rd approach
+# for when embedding Matplotlib in a GUI application
+# https://matplotlib.org/stable/gallery/index.html#user-interfaces
+
+
+# Suggestions for approach use:
+#   pyplot:     interactive plotting (e.g. in a Jupyter notebook)
+#   OO-style:   for non-interactive plotting (in functions and scripts intended to be reused, e.g. in a large project)
+
+
+# Pylab - discouraged and deprecated
+# from pylab import *  # imports everything from pyplot and numpy for a MATLAB style lab
+# x = linspace(0, 2 ,100)
+# plot(x, x, label='linear')
+# show
+
+
+# Plotting function
+# def my_plotter(ax, data1, data2, param_dict):
+#     out = ax.plot(data1, data2, **param_dict)
+#     return out
+#
+#
+# data1, data2, data3, data4 = np.random.randn(4, 100)
+# fig, ax = plt.subplots(1, 1)
+# my_plotter(ax, data1, data2, {'marker': 'x'})
+# plt.show()
+
+# fig, (ax1, ax2) = plt.subplots(1, 2)
+# my_plotter(ax1, data1, data2, {'marker': 'x'})
+# my_plotter(ax2, data3, data4, {'marker': 'o'})
+# plt.show()
+
+
+# Backends
